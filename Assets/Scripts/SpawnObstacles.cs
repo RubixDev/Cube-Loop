c6e4de1b347ef6d484aa88eeeab7fedd2049a293
RubixDev <silas.groh@t-online.de>
2019-06-15 16:58:03 +0200

    Added blue Power Up

--- Assets/Scripts/SpawnObstacles.cs
+++ Assets/Scripts/SpawnObstacles.cs
@@ -4,19 +4,21 @@ using Random = UnityEngine.Random;
 
 public class SpawnObstacles : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject obstacle;
+    public GameObject bluePowerUp;
     public float seconds = 4f;
     public PlayerMovement movement;
     public float zPos = 100f;
     public int minObstaclesPerRow = 4;
+    public float radius = 1f;
 
     void Start()
     {
-        StartCoroutine(Wait());
+        StartCoroutine(Loop());
     }
 
     
-    IEnumerator Wait()
+    IEnumerator Loop()
     {
         while (movement.enabled)
         {
@@ -28,21 +30,24 @@ public class SpawnObstacles : MonoBehaviour
     void SpawnRow()
     {
         float xPos = -6.4f;
-
         int range = Random.Range(minObstaclesPerRow, 6);
-        
+
+        Vector3 freePos = new Vector3(-6.4f, 1f, zPos);
+        int powerUp = Random.Range(0, 1);
+        bool powerUpPlaced = false;
+
         for (int i = 0; i < range; i++)
         {
-            int move = Random.Range(0, 2);
+            int move = Random.Range(0, 3);
 
-            if (move == 1)
+            if (move == 0)
             {
                 if (i != 0)
                 {
                     xPos += 2.1f;
                 }
             }
-            else
+            else if (move == 1)
             {
                 if (i != 0)
                 {
@@ -53,13 +58,32 @@ public class SpawnObstacles : MonoBehaviour
                     xPos += 2.1f;
                 }
             }
+            else
+            {
+                Instantiate(bluePowerUp, freePos, Quaternion.identity);
+            }
 
             if (xPos > 6.3f)
             {
                 break;
             }
-            
-            Instantiate(prefab, new Vector3(xPos, 1, zPos), Quaternion.identity);
+
+            Instantiate(obstacle, new Vector3(xPos, 1, zPos), Quaternion.identity);
+        }
+
+        if (powerUp == 0)
+        {
+            for (int n = 0; n < 7; n++)
+            {
+                int place = Random.Range(0, 2);
+                if (Physics.CheckSphere(freePos, radius) == false && powerUpPlaced == false && place == 0)
+                {
+                    Instantiate(bluePowerUp, freePos, Quaternion.identity);
+                    powerUpPlaced = true;
+                }
+
+                freePos.x += 2.1f;
+            }
         }
 
     }
@@ -72,11 +96,11 @@ public class SpawnObstacles : MonoBehaviour
 
         for (int i = 0; i < range; i++)
         {
-            Instantiate(prefab, new Vector3(xPos, 3, 10), Quaternion.identity);
+            Instantiate(obstacle, new Vector3(xPos, 3, 10), Quaternion.identity);
 
             xPos += 2.1f;
         }
 
-        Instantiate(prefab, new Vector3(0, 3, 5), Quaternion.identity);
+        Instantiate(obstacle, new Vector3(0, 3, 5), Quaternion.identity);
     }
 }
