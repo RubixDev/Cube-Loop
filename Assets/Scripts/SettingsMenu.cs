9628a2506171eb7d9fcec073b8aad76a76dbd136
RubixDev <silas.groh@t-online.de>
2019-06-17 18:33:23 +0200

    Added Options Menu

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Dropdown resolutionDropdown;
+    
+    private Resolution[] _resolutions;
+    private void Start()
+    {
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        var options = new List<string>();
+        var currentResolutionIndex = 0;
+        for (var i = 0; i < _resolutions.Length; i++)
+        {
+            var option = _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        var resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
